bcf839e97fe8a1ccc4cafb073ac611634ef8c9a2
Phuc Tran <glup3.tran@gmail.com>
2026-01-22 13:10:30 +0100

    add pocketbase loader

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection } from "astro:content";
+import { pocketbaseLoader } from "astro-loader-pocketbase";
+
+const cards = defineCollection({
+  loader: pocketbaseLoader({
+    url: import.meta.env.POCKETBASE_URL,
+    collectionName: "cards",
+    superuserCredentials: {
+      impersonateToken: import.meta.env.POCKETBASE_TOKEN,
+    },
+  }),
+});
+
+export const collections = { cards };
